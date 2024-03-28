0207676e0e2db7dbf4a25ca5ec26348efe7e80e5
Laura Vieira Pires <lauravieirapires@gmail.com>
2024-03-28 15:56:42 -0300

    Req 4, cria testes para show_details

--- tests/actions/test_show_details.py
+++ tests/actions/test_show_details.py
@@ -1,22 +1,28 @@
 from pro_filer.actions.main_actions import show_details  # NOQA
 import pytest
-from datetime import date
 
 
 @pytest.fixture
 def existing_file_context():
-    return {"base_path": "/path/to/existing_file.txt"}
+    return {"base_path": "images/pro-filer-preview.gif"}
 
 
 @pytest.fixture
 def non_existing_file_context():
-    return {"base_path": "/path/to/non_existing_file.txt"}
+    return {"base_path": "images/trybe/???"}
 
 
 def test_show_details_existing_file(capsys, existing_file_context):
     show_details(existing_file_context)
     captured = capsys.readouterr()
-    assert "File name: existing_file.txt" in captured.out
+    assert "File name: pro-filer-preview.gif" in captured.out
     assert "File size in bytes:" in captured.out
     assert "File type: file" in captured.out
-    assert "File extension: .txt" in captured.out
+    assert "File extension: .gif" in captured.out
+    assert "Last modified date:" in captured.out in captured.out
+
+
+def test_show_details_non_existing_file(capsys, non_existing_file_context):
+    show_details(non_existing_file_context)
+    captured = capsys.readouterr()
+    assert "File '???' does not exist\n" == captured.out
