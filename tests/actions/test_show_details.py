f4cea09134ab6e3b381c23aa86fd4e9a90e9c698
Laura Vieira Pires <lauravieirapires@gmail.com>
2024-03-28 20:46:26 -0300

    requisito 4 passando nos testes da trybe

--- tests/actions/test_show_details.py
+++ tests/actions/test_show_details.py
@@ -1,5 +1,6 @@
 from pro_filer.actions.main_actions import show_details  # NOQA
 import pytest
+from datetime import datetime
 
 
 @pytest.fixture
@@ -25,7 +26,52 @@ def test_show_details_existing_file(capsys, existing_file_context):
 def test_show_details_non_existing_file(capsys, non_existing_file_context):
     show_details(non_existing_file_context)
     captured = capsys.readouterr()
-    assert "File '???' does not exist\n" == captured.out
+    assert "File '???' does not exist\n" in captured.out
 
 
-# teste
+def test_show_details_without_extension(capsys):
+    context = {"base_path": "/home/trybe/Downloads/some_file"}
+    show_details(context)
+    captured = capsys.readouterr()
+    assert "File 'some_file' does not exist\n" in captured.out
+
+
+@pytest.fixture
+def file_without_extension(tmp_path):
+    file_path = tmp_path / "file_without_extension"
+    file_path.write_text("This is a test file without extension.")
+    return str(file_path)
+
+
+def test_show_details_file_without_extension(file_without_extension, capsys):
+    context = {"base_path": file_without_extension}
+    show_details(context)
+    captured = capsys.readouterr()
+    expected_output = (
+        "File name: file_without_extension\n"
+        "File size in bytes: 38\n"
+        "File type: file\n"
+        "File extension: [no extension]\n"
+        "Last modified date: 2024-03-28\n"
+    )
+    assert expected_output in captured.out
+
+
+def test_show_details_correct_date_format(capsys):
+    context = {"base_path": "pro_filer/actions/__init__.py"}
+    show_details(context)
+    captured = capsys.readouterr()
+    _, mod_date_str = captured.out.split("Last modified date: ")
+    try:
+        _ = datetime.strptime(mod_date_str.strip(), "%Y-%m-%d")
+    except ValueError:
+        pytest.fail("Formato de data incorreto")
+
+
+def test_show_details_incorrect_date_format(capsys):
+    context = {"base_path": "pro_filer/actions/__init__.py"}
+    show_details(context)
+    captured = capsys.readouterr()
+    _, mod_date_str = captured.out.split("Last modified date: ")
+    with pytest.raises(ValueError):
+        datetime.strptime(mod_date_str.strip(), "%d-%m-%Y")
