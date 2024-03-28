e4787c1f76320bd31fd4490949dfd9a78dd59247
Laura Vieira Pires <lauravieirapires@gmail.com>
2024-03-28 12:24:22 -0300

    Req 3, cria testes para show_preview e conserta erros de linter

--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -27,3 +27,27 @@ def context_with_files_and_dirs():
 @pytest.fixture
 def context_with_empty_files_and_dirs():
     return {"all_files": [], "all_dirs": []}
+
+
+def test_show_preview_with_files_and_dirs(capsys, context_with_files_and_dirs):
+    show_preview(context_with_files_and_dirs)
+    captured = capsys.readouterr()
+    assert "Found 6 files and 6 directories" in captured.out
+    assert (
+        "First 5 files: ['/path/to/file1.txt', '/path/to/file2.txt', "
+        "'/path/to/file3.txt', '/path/to/file4.txt', '/path/to/file5.txt']"
+        in captured.out
+    )
+    assert (
+        "First 5 directories: ['/path/to/directory1', '/path/to/directory2', "
+        "'/path/to/directory3', '/path/to/directory4', '/path/to/directory5']"
+        in captured.out
+    )
+
+
+def test_show_preview_with_empty_files_and_dirs(
+    capsys, context_with_empty_files_and_dirs
+):
+    show_preview(context_with_empty_files_and_dirs)
+    captured = capsys.readouterr()
+    assert "Found 0 files and 0 directories" in captured.out
